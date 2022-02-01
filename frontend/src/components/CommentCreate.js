f4002f78f22ff37ef1b0bb80991ea65821095503
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2022-01-31 21:44:23 -0600

    rename client dir

---
+++ frontend/src/components/CommentCreate.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import axios from "axios";
+// import { useParams } from 'react-router-dom'
+
+const CommentCreate = ({ postId }) => {
+  // const postId = useParams('id').id
+
+  const [comment, setComment] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    // send new post
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content: comment,
+    });
+
+    // reset title
+    setComment("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="form-control"
+          />
+        </div>
+
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
---
+++ frontend/src/components/PostCreate.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    // send new post
+    await axios.post("http://localhost:4000/posts", { title });
+
+    // reset title
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
